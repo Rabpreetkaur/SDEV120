08e8e36855c7d733f789b4de1a1bfe4694177da4
Rabpreetkaur <rkaur65@ivytech.edu>
2024-09-29 00:50:16 -0400

    Add files via upload

---
+++ raining_cats_and_dogs.py
@@ -0,0 +1,10 @@
+# This script prints the sentence "It is raining cats and dogs"
+# Variables to store the words "cats" and "dogs"
+c = "cats"  # Set variable 'c' to "cats"
+d = "dogs"  # Set variable 'd' to "dogs"
+
+# Constructing the sentence with variables
+s = "It is raining " + c + " and " + d + "."  # Set 's' to the sentence using concatenation
+
+# Printing the sentence
+print(s)  # Output the sentence
