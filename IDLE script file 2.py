1cc9d4cf1bd7bdd2ce29dfd48e3a884a515d843f
Rabpreetkaur <rkaur65@ivytech.edu>
2024-10-12 19:23:52 -0400

    Add files via upload

---
+++ IDLE script file 2.py
@@ -0,0 +1,23 @@
+# Define the greaterThan function
+def greaterThan(x, y):
+    return x > y
+
+# Assign values to variables
+a = 2
+b = 3
+
+# Call the function and store the result
+result = greaterThan(a, b)
+
+# Print the output statement
+print("The statement", str(a), "is greater than", str(b), "is", str(result))
+
+# Testing the second scenario
+a = 10
+b = 6
+
+# Call the function again and store the result
+result = greaterThan(a, b)
+
+# Print the new output statement
+print("The statement", str(a), "is greater than", str(b), "is", str(result))
